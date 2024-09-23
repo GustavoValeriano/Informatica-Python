02c723875ce3a0cc5d269b8e0c4bd0685aab613a
GustavoValeriano <gustavovaleriano24@gmail.com>
2024-09-23 20:54:59 -0300

    Nome, idade, altura, estudante

---
+++ main (1).py
@@ -0,0 +1,11 @@
+# Declarando variáveis
+nome = "Gustavo"       # String
+idade = 17             # Inteiro
+altura = 1.79          # Float
+estudante = True       # Booleano
+
+# Exibindo as variáveis
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Estudante:", estudante)
